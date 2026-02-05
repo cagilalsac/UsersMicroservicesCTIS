687be2e59f55e978b837b3239fe12f378af0c7ee
Çağıl Alsaç <cagilalsac@outlook.com>
2026-02-05 12:24:09 +0300

    Response, CommandResponse and ServiceBase classes added in CORE. Culture management covered.

---
+++ CORE/APP/Services/ServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using CORE.APP.Models;
+using System.Globalization;
+
+namespace CORE.APP.Services
+{
+    public abstract class ServiceBase
+    {
+        private CultureInfo _cultureInfo; // field
+
+        protected CultureInfo CultureInfo // property
+        { 
+            get
+            {
+                return _cultureInfo;
+            }
+            set
+            {
+                _cultureInfo = value;
+                Thread.CurrentThread.CurrentCulture = _cultureInfo;
+                Thread.CurrentThread.CurrentUICulture = _cultureInfo;
+            }
+        }
+
+        protected ServiceBase()
+        {
+            CultureInfo = new CultureInfo("en-US"); // tr-TR
+        }
+
+        protected CommandResponse Success(string message, int id) => new CommandResponse(true, message, id);
+
+        protected CommandResponse Error(string message) => new CommandResponse(false, message);
+    }
+}
