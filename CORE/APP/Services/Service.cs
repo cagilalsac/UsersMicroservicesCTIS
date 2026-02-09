fccb6cf19d0b9535b57786e9bf7212583e592862
Çağıl Alsaç <cagilalsac@outlook.com>
2026-02-09 16:23:26 +0300

    Generic Service class created.

---
+++ CORE/APP/Services/Service.cs
@@ -0,0 +1,12 @@
+﻿using CORE.APP.Domain;
+
+namespace CORE.APP.Services
+{
+    public abstract class Service<TEntity> : ServiceBase, IDisposable where TEntity : Entity, new()
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CORE/APP/Services/ServiceBase.cs
+++ CORE/APP/Services/ServiceBase.cs
@@ -21,13 +21,16 @@ namespace CORE.APP.Services
             }
         }
 
-        protected ServiceBase()
+        protected ServiceBase() // constructor
         {
             CultureInfo = new CultureInfo("en-US"); // tr-TR
         }
 
-        protected CommandResponse Success(string message, int id) => new CommandResponse(true, message, id);
+        // If needed, culture can be changed in a child class as below:
+        // CultureInfo = new CultureInfo("tr-TR");
 
-        protected CommandResponse Error(string message) => new CommandResponse(false, message);
+        protected CommandResponse Success(string message, int id) => new CommandResponse(true, message, id); // behavior
+
+        protected CommandResponse Error(string message) => new CommandResponse(false, message); // behavior
     }
 }
