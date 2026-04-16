d3feef83710f543e1d0ef34d0222c70d5c72aab7
Çağıl Alsaç <cagilalsac@outlook.com>
2026-04-16 12:19:06 +0300

    JWT generation completed.

---
+++ Users.API/Controllers/TokensController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Users.APP.Features.Tokens;
+
+namespace Users.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IConfiguration _configuration;
+
+        public TokensController(IMediator mediator, IConfiguration configuration)
+        {
+            _mediator = mediator;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Token(TokenRequest request)
+        {
+            request.SecurityKey = _configuration["SecurityKey"];
+            request.Issuer = _configuration["Issuer"];
+            request.Audience = _configuration["Audience"];
+            if (ModelState.IsValid)
+            {
+                var response = await _mediator.Send(request);
+                if (response is null)
+                    return BadRequest("User not found!");
+                return Ok(response);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
--- Users.API/Program.cs
+++ Users.API/Program.cs
@@ -1,4 +1,8 @@
+using CORE.APP.Services.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Users.APP.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +19,30 @@ foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
 {
     builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(assembly));
 }
+
+builder.Services.AddSingleton<ITokenAuthService, TokenAuthService>();
+
+builder.Configuration["SecurityKey"] = "users_microservices_security_key_2026=";
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(config =>
+    {
+        // Define rules for validating JWT.
+        config.TokenValidationParameters = new TokenValidationParameters
+        {
+            // Use the builder configuration's security key to create a new symmetric security key for verifying the JWT's signature.
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecurityKey"] ?? string.Empty)),
+
+            ValidIssuer = builder.Configuration["Issuer"], // get Issuer section's value from appsettings.json
+            ValidAudience = builder.Configuration["Audience"], // get Audience section's value from appsettings.json
+
+            // These flags ensure the validation of the JWT.
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -35,6 +63,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
