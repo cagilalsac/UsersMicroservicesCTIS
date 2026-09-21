5c9d2978e95ba55dfea1c9473a19686c7b262bcb
Çağıl Alsaç <cagilalsac@outlook.com>
2026-09-21 15:21:27 +0300

    Add project files.

---
+++ CORE/Domain/Record.cs
@@ -0,0 +1,30 @@
+﻿namespace CORE.Domain
+{
+    public abstract class Record
+    {
+        // Way 1:
+        //private int id; // field
+
+        //public void setId(int id) // behavior
+        //{
+        //    this.id = id;
+        //}
+
+        //public int getId()
+        //{
+        //    return id;
+        //}
+
+        // Way 2:
+        public int Id { get; set; } // property
+
+        protected Record()
+        {
+        }
+
+        protected Record(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ CORE/Models/CommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using CORE.Domain;
+
+namespace CORE.Models
+{
+    public class CommandResponse : Record
+    {
+        public bool IsSuccessful { get; } // readonly
+
+        public string Message { get; } // readonly
+
+        public CommandResponse(bool isSuccessful, string message, int id = 0) : base(id)
+        {
+            IsSuccessful = isSuccessful;
+            Message = message;
+        }
+    }
+}
